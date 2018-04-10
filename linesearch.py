2a2be4ca9c7d1d5bf06cbcc7a13d940ea161c823
Caparrini <a.caparrini@icloud.com>
2018-04-10 21:54:35 +0200

    Added other linesearch method

--- linesearch.py
+++ linesearch.py
@@ -3,6 +3,186 @@ import cv2
 import matplotlib.pyplot as plt
 import imagetransform
 
+class Tracker():
+    """I detected lane lines from the warped image using a sliding-window-based tracking method
+    (credit: Tutorial Video on P4 by Udacity). This involves firstly summing the vertical pixel
+    values in the image and identifying the 2 peaks in the histogram, which should give an
+    indication of the location of the lane lines.
+
+    We identify the centers of these peaks and start at the corresponding point in the warped
+    image. We then move up and down, and left and right within certain acceptable limits,
+    identifying the next possible set of pixels that might form part of the lane line.
+    This is done in the method below (credit: P4 Tutorial Video, Udacity)
+    https://github.com/RWMostert/Udacity-SDCND---P4-Advanced-Lane-Lines
+    """
+    def __init__(self, Mywindow_width, Mywindow_height, Mymargin, Mysmooth_factor=15):
+
+        self.recent_centers = []
+        self.window_width = Mywindow_width
+        self.window_height = Mywindow_height
+        self.margin = Mymargin
+        self.smooth_factor = Mysmooth_factor
+
+    def find_window_centroids(self, warped):
+
+        window_width = self.window_width
+        window_height = self.window_height
+        margin = self.margin
+
+        window_centroids = []
+        window = np.ones(window_width)
+
+        l_sum = np.sum(warped[int(3 * warped.shape[0] / 4):, :int(warped.shape[1] / 2)], axis=0)
+        l_center = np.argmax(np.convolve(window, l_sum)) - window_width / 2
+        r_sum = np.sum(warped[int(3 * warped.shape[0] / 4):, int(warped.shape[1] / 2):], axis=0)
+        r_center = np.argmax(np.convolve(window, r_sum)) - window_width / 2 + int(warped.shape[1] / 2)
+
+        window_centroids.append((l_center, r_center))
+
+        for level in range(1, (int)(warped.shape[0] / window_height)):
+            image_layer = np.sum(
+                warped[int(warped.shape[0] - (level + 1) * window_height):int(warped.shape[0] - level * window_height),
+                :], axis=0)
+            conv_signal = np.convolve(window, image_layer)
+
+            offset = window_width / 2
+            l_min_index = int(max(l_center + offset - margin, 0))
+            l_max_index = int(min(l_center + offset + margin, warped.shape[1]))
+            l_center = np.argmax(conv_signal[l_min_index:l_max_index]) + l_min_index - offset
+
+            r_min_index = int(max(r_center + offset - margin, 0))
+            r_max_index = int(min(r_center + offset + margin, warped.shape[1]))
+            r_center = np.argmax(conv_signal[r_min_index: r_max_index]) + r_min_index - offset
+
+            window_centroids.append((l_center, r_center))
+
+        self.recent_centers.append(window_centroids)
+
+        return np.average(self.recent_centers[-self.smooth_factor:], axis=0)
+
+    def detect_lines(self, warped):
+
+        window_centroids = self.find_window_centroids(warped)
+
+        l_points = np.zeros_like(warped)
+        r_points = np.zeros_like(warped)
+
+        leftx = []
+        rightx = []
+
+        for level in range(0, len(window_centroids)):
+            leftx.append(window_centroids[level][0])
+            rightx.append(window_centroids[level][1])
+
+            l_mask = window_mask(self.window_width, self.window_height, warped, window_centroids[level][0], level)
+            r_mask = window_mask(self.window_width, self.window_height, warped, window_centroids[level][1], level)
+
+            l_points[(l_points == 255) | ((l_mask == 1))] = 255
+            r_points[(r_points == 255) | ((r_mask == 1))] = 255
+
+        template = np.array(r_points + l_points, np.uint8)
+        zero_channel = np.zeros_like(template)
+        template = np.array(cv2.merge((zero_channel, template, zero_channel)), np.uint8)
+        warpage = np.array(cv2.merge((warped, warped, warped)), np.uint8)
+        detected_lanes = cv2.addWeighted(warpage, 1, template, 0.5, 0.0)
+
+        return detected_lanes, leftx, rightx
+
+class Lane_fitter():
+    """
+    https://github.com/RWMostert/Udacity-SDCND---P4-Advanced-Lane-Lines
+    """
+    def __init__(self):
+        self.previous_left_MSE = float('inf')
+        self.previous_right_MSE = float('inf')
+        self.previous_left_poly = np.zeros(shape=(3))
+        self.previous_right_poly = np.zeros(shape=(3))
+        self.previous_7_left_lanes = []
+        self.previous_7_right_lanes = []
+
+    def fit_lanes(self, current_image, window_width, window_height, leftx, rightx):
+        history_lines = 15
+        if len(self.previous_7_left_lanes) > history_lines:
+            self.previous_7_left_lanes = self.previous_7_left_lanes[-history_lines:]
+        if len(self.previous_7_right_lanes) > history_lines:
+            self.previous_7_right_lanes = self.previous_7_right_lanes[-history_lines:]
+
+        # Next, I fit a polynomial to the curve:
+
+        yvals = range(0, current_image.shape[0])
+        res_yvals = np.arange(current_image.shape[0] - (window_height / 2), 0, -window_height)
+
+        left_fit, left_res, _, _, _ = np.polyfit(res_yvals, leftx, 2, full=True)
+        right_fit, right_res, _, _, _ = np.polyfit(res_yvals, rightx, 2, full=True)
+
+        self.previous_7_left_lanes.append(left_fit)
+        self.previous_7_right_lanes.append(right_fit)
+
+        left_fit = np.average(self.previous_7_left_lanes, axis=0)
+        right_fit = np.average(self.previous_7_right_lanes, axis=0)
+
+        # I used the MSE of the fit to inform a weighted average across successive frames.
+        """
+        MSE = np.mean(np.square(left_res[1:] - right_res[1:]))
+
+        #Bayesian informing
+        k = MSE/(MSE + self.previous_left_MSE)
+        left_fit = k*self.previous_left_poly + (1-k)*left_fit
+
+        k = MSE/(MSE + self.previous_right_MSE)
+        right_fit = k*self.previous_right_poly + (1-k)*right_fit
+
+        if self.previous_right_MSE < float('inf'):
+            self.previous_right_MSE = k*self.previous_right_MSE + (1-k)*MSE
+        else:
+            self.previous_right_MSE = MSE   
+        self.previous_right_poly = right_fit
+
+        if self.previous_left_MSE < float('inf'):
+            self.previous_left_MSE = k*self.previous_left_MSE + (1-k)*MSE
+        else:
+            self.previous_left_MSE = MSE   
+        self.previous_left_poly = left_fit"""
+
+        left_fitx = left_fit[0] * yvals * yvals + left_fit[1] * yvals + left_fit[2]
+        left_fitx = np.array(left_fitx, np.int32)
+
+        right_fitx = right_fit[0] * yvals * yvals + right_fit[1] * yvals + right_fit[2]
+        right_fitx = np.array(right_fitx, np.int32)
+
+        left_lane = np.array(list(
+            zip(np.concatenate((left_fitx - window_width / 2, left_fitx[::-1] + window_width / 2), axis=0),
+                np.concatenate((yvals, yvals[::-1]), axis=0))), np.int32)
+        right_lane = np.array(list(
+            zip(np.concatenate((right_fitx - window_width / 2, right_fitx[::-1] + window_width / 2), axis=0),
+                np.concatenate((yvals, yvals[::-1]), axis=0))), np.int32)
+        inner_lane = np.array(list(
+            zip(np.concatenate((left_fitx + window_width / 2, right_fitx[::-1] - window_width / 2), axis=0),
+                np.concatenate((yvals, yvals[::-1]), axis=0))), np.int32)
+
+        # Assuming the lane is about 30 meters long and 3.7 meters wide:
+        ym_per_pix = 30 / 720  # meters per pixel in y dimension
+        xm_per_pix = 3.7 / 720  # meters per pixel in x dimension
+
+        # Firstly I fit a second order polynomial (curve) to the left lane. I then used this fitted curve to calculate the angle of the left lane line's curvature.
+
+        corner_curve = np.polyfit(x=np.array(res_yvals, np.float32) * ym_per_pix,
+                                  y=np.array(leftx, np.float32) * xm_per_pix,
+                                  deg=2)
+        # curve_radians = ((1 + (2*corner_curve[0]*np.max(yvals[-1]*ym_per_pix + corner_curve[1])**2)**1.5) / np.absolute(2*corner_curve[0])
+        curve_radians = ((1 + (
+        2 * corner_curve[0] * np.max(yvals) * ym_per_pix + corner_curve[1]) ** 2) ** 1.5) / np.absolute(
+            2 * corner_curve[0])
+
+        camera_center = (left_fitx[-1] + right_fitx[-1]) / 2
+        center_diff = (camera_center - current_image.shape[1] / 2) * xm_per_pix
+
+        side_pos = 'left'
+        if center_diff <= 0:
+            side_pos = "right"
+
+        return left_lane, inner_lane, right_lane, curve_radians, center_diff, side_pos
+
 def fitpolynomial(binary_warped):
     # Assuming you have created a warped binary image called "binary_warped"
     # Take a histogram of the bottom half of the image
